aa8fd0d9a80603391ce4003a07122f378062d442
Ayoub <boulaidayoub02@gmail.com>
2023-08-20 13:28:45 -0700

    My printf

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <unistd.h>
+
+#define BUFF_SIZE 1024 /* Define the buffer size as needed */
+
+int _printf(const char *format, ...);
+
+#endif /* MAIN_H */
+
